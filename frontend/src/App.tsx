ca8de0540e930c97469bf44714e0333557dbf289
PABLODANTAS2021 <pablodesouzadantas@gmail.com>
2021-11-02 19:09:10 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from "assests/Components/DataTable";
+import Footer from "assests/Components/Footer";
+import NavBar from "assests/Components/NavBar/NavBar";
+
+
 function App() {
   return (
-
-    <div>
-      <h1 className="text-primary">Ola mundo!</h1>
-    </div>
-
+    <>
+      <NavBar/>
+      <div className="container">
+        <h1 className="text-primary">Ola mundo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+   </>
 
   );
 }
---
+++ frontend/src/assests/Components/Footer/index.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/PABLODANTAS2021" target="_blank" rel="PABLODANTAS2021">Pablo Dantas</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+
+
+
+    );
+}
+
+export default Footer;
